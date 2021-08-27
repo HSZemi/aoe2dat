5925dc28953409d9b25ae965ec4b577a6f8fb4ac
HSZemi <HSZemi@users.noreply.github.com>
2021-08-28 01:26:00 +0200

    Include Repeatable property for techs

--- code/tojson/tojson.cpp
+++ code/tojson/tojson.cpp
@@ -925,6 +925,7 @@ void research2json(std::ofstream& out, genie::Tech tech) {
 			<< std::endl;
 	out << "\"HotKey\":" << tech.HotKey << "," << std::endl;
 	out << "\"Name\":\"" << tech.Name << "\"," << std::endl;
-	out << "\"Name2\":\"" << tech.Name2 << "\"" << std::endl;
+	out << "\"Name2\":\"" << tech.Name2 << "\"," << std::endl;
+	out << "\"Repeatable\":\"" << std::to_string(tech.Repeatable) << "\"" << std::endl;
 	out << "}";
 }
